1b9e6615d218c31a8630b8201dcbf10aa342d9c9
Thanzeer <thanzeerwork@gmail.com>
2025-06-12 10:19:12 +0530

    footer added stable2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
+
 import Particles from '../../blocks/Backgrounds/Particles/Particles';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
-import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
 
   const [category,setCategory]= useState('All')
   return (
     <div className="relative w-full min-h-screen overflow-hidden pt-25">
-  
+      {/* particles as background */}
       <div className="absolute top-0 left-0 w-full h-full z-0 bg-black">
         <Particles
           particleColors={['#ffffff', '#ffffff']}
@@ -19,17 +19,18 @@ const Home = () => {
           speed={0.1}
           particleBaseSize={100}
           moveParticlesOnHover={true}
-          alphaParticles={true}
+          alphaParticles={false}
           disableRotation={false}
         />
       </div>
 
-      
-    <div className="relative z-10 sm:px-10 md:px-30 ">
+      {/* Foreground content */}
+      <div className="relative z-10  md:px-30 ">
         <Header />
         <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisplay category={category}/>
-        
+       
+        {/* You can add more content here */}
       </div>
     </div>
   );
